b873080b3eeaf92471827e0ccacc843fd9670b21
wsierra010 <wsierra010@gmail.com>
2021-04-19 12:54:01 +0200

    Create Account Page

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -5,9 +5,52 @@ import Header from "../../components/Header";
 
 function Account() {
     return (
-        <main className="main">
-            <div>
-                <img src="http://dummyimage.com/50x50/fff/000.gif&text=Profile"></img>
+        <main className="main_container">
+            <div className="main_container__profileImg">
+                <img
+                src="http://dummyimage.com/50x50/fff/000.gif&text=Profile"
+
+                ></img>
+                <button>Change Image</button>
+            </div>
+            <div className="main_container__nav">
+                <button>Personal information</button>
+                <button>Change Password</button>
+            </div>
+            <div className="main_container__profileInfo">
+                <article className="main_container__formInfo">
+                    <table className="main_container_profileInfo__table">
+                        <tbody>
+                            <tr className="main_container_profileInfo__data">
+                                <td>First Name</td>
+                                <td>Wilmer</td>
+                            </tr>
+                            <tr className="main_container_profileInfo__data">
+                                <td>Last Name</td>
+                                <td>Sierra Salgado</td>
+                            </tr>
+                            <tr className="main_container_profileInfo__data">
+                                <td>Email</td>
+                                <td>wsierra010@gmail.com</td>
+                            </tr>
+                            <tr className="main_container_profileInfo__data">
+                                <td>Phone Number</td>
+                                <td>677 77 77 77</td>
+                            </tr>
+                            <tr className="main_container_profileInfo__data">
+                                <td>Gender</td>
+                                <td>Male</td>
+                            </tr>
+                            <tr className="main_container_profileInfo__data">
+                                <td>Country</td>
+                                <td>Spain</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </article>
+                <article className="main_container_profileInfo_btn">
+                    <button className="main_container_profileInfo_btn__tag">Edit information</button>
+                </article>
             </div>
         </main>
     );
