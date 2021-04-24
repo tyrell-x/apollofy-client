29684be5b8edf12b970d334f1c63609ec4c71978
Pablo Caravaca <pablocaravacacalvo@gmail.com>
2021-04-24 19:20:15 +0200

    remove flselect from home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,6 @@ import { useSelector } from "react-redux";
 import "./Home.scss";
 import Navbar from "../../components/Navbar";
 import { authSelector } from "../../redux/auth/auth-selectors";
-import FLSelect from "../../components/FLSelect/FLSelect.js";
-import { useState } from "react";
 
 function Home() {
   const { currentUser } = useSelector(authSelector);
@@ -15,34 +13,6 @@ function Home() {
         <section>
           <div>
             <h1>Hello {currentUser.firstName || currentUser.email}</h1>
-            <FLSelect
-              style={{
-                width: 200,
-              }}
-              label="select"
-              options={[
-                {
-                  label: "Option 1",
-                  value: "option1",
-                },
-                {
-                  label: "Option 2",
-                  value: "option1",
-                },
-                {
-                  label: "Option 3",
-                  value: "option1",
-                },
-                {
-                  label: "Option 4",
-                  value: "option1",
-                },
-                {
-                  label: "Option 5",
-                  value: "option1",
-                },
-              ]}
-            ></FLSelect>
           </div>
         </section>
       </main>
