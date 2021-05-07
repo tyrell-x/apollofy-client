6e31b8bfd1eb804bb5aafa5b0e0c5f624f77fe8f
Leyber91 <leyber.91.2@gmail.com>
2021-05-07 13:48:52 +0200

    DragAndDropReconstruction succesfull

--- src/pages/Playlist/Playlist.js
+++ src/pages/Playlist/Playlist.js
@@ -1,5 +1,5 @@
 import "./Playlist.scss";
-
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { selectPlaylist } from "../../redux/playlists/playlists-selectors.js";
@@ -10,21 +10,14 @@ import {
   faPlay
 } from "@fortawesome/free-solid-svg-icons";
 import { setTracksInPlayer } from "../../redux/player/player-actions.js";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "../../services/auth/auth.js";
 import { fetchAllPlaylists } from "../../redux/playlists/playlists-actions.js";
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const defaultImage =
   "https://i.pinimg.com/originals/f8/65/d3/f865d3112022612c6875b4ab7ec54239.jpg";
 
 function Playlist() {
-
-  // const trackList = playlist.tracks
-  // // console.log(trackList)
-
-  // const [tracks, updateTracks] = useState(trackList);
-  
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -38,21 +31,24 @@ function Playlist() {
   const { id } = useParams();
 
   const playlist = useSelector(selectPlaylist(id));
-  const [tracks, updateTracks] = useState(playlist.tracks);
-  console.log(playlist.tracks)
+  const initialTracksPlaylist = playlist.tracks
+  const [tracksPlaylist, updatePlaylistOrder] = useState(playlist.tracks);
+
+  console.log(initialTracksPlaylist)
 
   const playPlaylist = () => {
     dispatch(setTracksInPlayer(playlist.tracks.map(track => track._id)))
   }
 
+  //UPDATE on Drag End
   function handleOnDragEnd(result) {
     if (!result.destination) return;
-    console.log(result)
-    const items = Array.from(tracks);
+
+    const items = Array.from(tracksPlaylist);
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem);
-    console.log(result.destination.index)
-    updateTracks(items);
+
+    updatePlaylistOrder(items);
   }
 
   return (
@@ -72,49 +68,40 @@ function Playlist() {
           </div>
         </div>
       </div>
-      <div className="tracks">
+      <h2>The list</h2>
+
       <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="tracks">
+        <Droppable droppableId="playlist">
           {(provided) => (
-              <div className="tracks" {...provided.droppableProps} ref={provided.innerRef}>
-                {playlist.tracks.map((track, index) => {
-                  return (
-                  <Draggable key={id} draggableId={id} index={index}>
-                        {(provided) => (
-                          <div 
-                            key={track._id} 
-                            className="track"  
-                            ref={provided.innerRef} 
-                            {...provided.draggableProps} 
-                            {...provided.dragHandleProps}>
-                            <div>
-                            
-                              <div className="image">
-                                <img src={track.thumbnail} alt="track" height="100%"></img>
-                              </div>
+            <div className="tracks" {...provided.droppableProps} ref={provided.innerRef}>
+              {playlist.tracks.map((track, index) => (
+                // DIV OF THE TRACK
+                <Draggable key={track._id} draggableId={track._id} index={index}>
+                  {(provided) => (
+                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      <div className="image">
+                        <img src={track.thumbnail} alt="track" height="100%"></img>
+                      </div>
+                      <div className="details">
+                        <div className="title">{track.title}</div>
+                        <div className="artist">{track.artist || "anonymous"}</div>
+                      </div>
+                      <div className="owner">{track.owned ? "Owned" : ""}</div>
+                      <div className="duration">{getCounter(track.duration)}</div>
+                      <div className="actions">
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
 
-                              <div className="details">
-                                <div className="title">{track.title}</div>
-                                <div className="artist">{track.artist || "anonymous"}</div>
-                              </div>
-
-                              <div className="owner">{track.owned ? "Owned" : ""}</div>
-                              <div className="duration">{getCounter(track.duration)}</div>
-                              <div className="actions"></div>
-
-                            </div>
-                          </div>
-                        )}
-                    </Draggable>
-                      )
-                    })}
-                  {provided.placeholder}
-                </div>
-          )};
+                // DIV OF THE TRACK
+              ))}
+            </div>
+          )}
         </Droppable>
-        </DragDropContext>
-      </div>
-    </div>
+      </DragDropContext>
+      
+    </div> //PLAYLIST PAGE CLOSING DIV
   );
 }
 
