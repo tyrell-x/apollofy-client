2b4b61d6ac6bb9dbb4636fb03075df5a590908e4
wsierra010 <wsierra010@gmail.com>
2021-05-13 22:36:32 +0200

    refactor update user

--- src/pages/Account/sections/EditAccount/EditAccount.js
+++ src/pages/Account/sections/EditAccount/EditAccount.js
@@ -1,22 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { updateUserAccount, updateUserAccountError } from "../../../../redux/auth/auth-actions";
-import { useForm } from "react-hook-form";
 
 import Button from "../../../../components/Button";
 import FLInput from "../../../../components/FLInput";
 import "./EditAccount.scss";
 import { useSelector } from "react-redux";
 import { selectProfileState } from "../../../../redux/auth/auth-selectors"
-import FLSelect from "../../../../components/FLSelect/FLSelect";
 
 function EditAccount() {
   const dispatch = useDispatch();
-
-  useEffect(() => {});
-  const {
-    handleSubmit,
-  } = useForm();
 
   const user = useSelector(selectProfileState);
 
@@ -31,19 +24,20 @@ function EditAccount() {
     });
   };
 
-  const onSubmit = (data) => {
-    const { ...userData } = data;
-    dispatch(updateUserAccount(userData));
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(updateUserAccount(data));
   };
 
 
   return (
     <section className="edit-account">
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <form className="form" onSubmit={onSubmit}>
         <FLInput
           name="firstName"
-          value={data?.firstName || ""}
           onChange={handleInput}
+          value={data.firstName || ""}
           label="First Name"
         />
         <FLInput
--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -3,9 +3,11 @@ import { createSelector } from "reselect";
 export const selectAuthState = (state) => state.auth;
 export const selectProfileState = (state) => state.auth.currentUser;
 
+
 export const authSelector = createSelector([selectAuthState], (auth) => auth);
 
 export const profileSelector = createSelector(
   [selectProfileState],
   (currentUser) => currentUser,
 );
+
